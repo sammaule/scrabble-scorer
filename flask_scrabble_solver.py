9ca9b490464ab08b91c2379b72112a264a15a362
Sam Maule <sam.maule@gmail.com>
2019-11-26 21:49:05 +0000

    Added game front end screen and interaction between start game and main game page.

--- flask_scrabble_solver.py
+++ flask_scrabble_solver.py
@@ -1,13 +1,26 @@
-from flask import Flask, render_template, url_for
-from forms import NewGameForm
+from flask import Flask, render_template, url_for, flash, redirect
+
+from forms import NewGameForm, MidGameForm
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bfgsb4321fdsdfs42hdhfjd82898ba245'
 
-@app.route("/")
+# Pre game class
+@app.route("/", methods=['GET', 'POST'])
 @app.route("/home", methods=['GET', 'POST'])
-def home():
+def start_game():
 	form = NewGameForm()
-	return render_template('home.html', form=form)
+	if form.validate_on_submit():
+		flash('Starting Game!', 'success')
+		return redirect(url_for('game'))
+	return render_template('start_game.html', form=form)
+
+# Mid game class
+@app.route("/game", methods=['GET', 'POST'])
+def game():
+	form = MidGameForm()
+	return render_template('game.html', form=form)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
--- forms.py
+++ forms.py
@@ -5,14 +5,27 @@ from wtforms.validators import DataRequired, NumberRange
 
 class NewGameForm(FlaskForm):
     num_players = IntegerField('Number of Players',
-                           validators=[DataRequired(), NumberRange(min=2, max=4,
-                            message = '2 - 4 players only.')])
+                               validators=[DataRequired(),
+                                           NumberRange(min=2, max=4, message='2 - 4 players only.')])
     player_1_name = StringField('Player 1 Name',
-                        validators=[])
+                                validators=[])
     player_2_name = StringField('Player 2 Name',
-                        validators=[])
+                                validators=[])
     player_3_name = StringField('Player 3 Name',
-                        validators=[])
+                                validators=[])
     player_4_name = StringField('Player 4 Name',
-                        validators=[])
+                                validators=[])
     submit = SubmitField('Start Game')
+
+
+class MidGameForm(FlaskForm):
+    player_1_score = IntegerField('Player 1 score:',
+                                  validators=[DataRequired()])
+    player_2_score = IntegerField('Player 2 score:',
+                                  validators=[DataRequired()])
+    player_3_score = IntegerField('Player 1 score:',
+                                  validators=[DataRequired()])
+    player_4_score = IntegerField('Player 4 score:',
+                                  validators=[DataRequired()])
+    submit = SubmitField('Update Scores')
+    end_game = SubmitField('End Game')
