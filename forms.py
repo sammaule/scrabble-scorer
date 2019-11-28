74840a0cc31804542d90378562b56088a2b8908a
smaule <smaule@deloitte.co.uk>
2019-11-28 19:56:22 +0000

    Added functionality to dynamically display number of players in the game field

--- forms.py
+++ forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
+from wtforms import StringField, IntegerField, SubmitField, SelectField
 from wtforms.validators import DataRequired, NumberRange
 
 
 class NewGameForm(FlaskForm):
-    num_players = IntegerField('Number of Players',
-                               validators=[DataRequired(),
-                                           NumberRange(min=2, max=4, message='2 - 4 players only.')])
+    num_players = SelectField('Number of Players',
+                              validators=[DataRequired()],
+                              choices=[('2', '2'), ('3', '3'), ('4', '4')])
     player_1_name = StringField('Player 1 Name',
                                 validators=[])
     player_2_name = StringField('Player 2 Name',
@@ -18,14 +18,39 @@ class NewGameForm(FlaskForm):
     submit = SubmitField('Start Game')
 
 
-class MidGameForm(FlaskForm):
+class TwoPlayerGameForm(FlaskForm):
+
     player_1_score = IntegerField('Player 1 score:',
                                   validators=[DataRequired()])
     player_2_score = IntegerField('Player 2 score:',
                                   validators=[DataRequired()])
-    player_3_score = IntegerField('Player 1 score:',
+    submit = SubmitField('Update Scores')
+    end_game = SubmitField('End Game')
+
+
+class ThreePlayerGameForm(FlaskForm):
+
+    player_1_score = IntegerField('Player 1 score:',
+                                  validators=[DataRequired()])
+    player_2_score = IntegerField('Player 2 score:',
+                                  validators=[DataRequired()])
+    player_3_score = IntegerField('Player 3 score:',
+                                  validators=[DataRequired()])
+
+    submit = SubmitField('Update Scores')
+    end_game = SubmitField('End Game')
+
+
+class FourPlayerGameForm(FlaskForm):
+
+    player_1_score = IntegerField('Player 1 score:',
+                                  validators=[DataRequired()])
+    player_2_score = IntegerField('Player 2 score:',
+                                  validators=[DataRequired()])
+    player_3_score = IntegerField('Player 3 score:',
                                   validators=[DataRequired()])
     player_4_score = IntegerField('Player 4 score:',
                                   validators=[DataRequired()])
+
     submit = SubmitField('Update Scores')
     end_game = SubmitField('End Game')
